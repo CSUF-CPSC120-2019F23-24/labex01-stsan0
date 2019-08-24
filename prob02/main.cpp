da82dd7b5d1cef02fc31aed6e0af7d38fdb5d7de
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-08-24 14:12:10 -0700

    changed prob02 quotations

--- prob02/main.cpp
+++ prob02/main.cpp
@@ -7,18 +7,18 @@ int main()
   double hours, rate, pay;
 
   // Get the number of hours worked.
-  std::cout << "How many hours did you work? ";
+  std::cout << "How many bars of candy did you sell? ";
   std::cin >> hours;
 
   // Get the hourly pay rate.
-  std::cout << "How much do you get paid per hour? ";
+  std::cout << "How much does the organization earn per bar sold? ";
   std::cin >> rate;
 
   // Calculate the pay.
   pay = hours * rate;
 
   // Display the pay.
-  std::cout << "You have earned $" << pay << std::endl;
+  std::cout << "The organization, thanks to you, has earned $" << pay << "! Keep up the good work!" << std::endl ;
 
   return 0;
 }
