0cd2dc012d3f83c9b5926c14b64d0b21619b6104
stsan <stsan@cs104u-1e.acad.fullerton.edu>
2019-08-24 13:44:48 -0700

    changed hello world message

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
   // Print the message to the console.
-  std::cout << "Hello world!!" << std::endl;
+  std::cout << "mitochondria is the powerhouse of the cell!" << std::endl;
 
   return 0;
 }
